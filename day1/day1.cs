cc28d539c3e6f501b9fb92ca177c3909997c2d15
ronhaland <ronnyhaland@gmail.com>
2025-12-02 14:18:58 +0100

    day1 ⭐️⭐️

--- day1/day1.cs
+++ day1/day1.cs
@@ -21,3 +21,38 @@ foreach (var rotation in content)
 }
 System.Console.WriteLine("--------- PART 1 --------");
 System.Console.WriteLine(count);
+System.Console.WriteLine("-------------------------");
+
+
+
+position = 50;
+count = 0;
+
+foreach (var rotation in content)
+{
+  var diff = rotation[0] switch
+  {
+    'L' => -int.Parse(rotation[1..]),
+    'R' => int.Parse(rotation[1..]),
+    _ => 0,
+  };
+
+  var fullRotations = Math.Abs(diff / 100);
+  count += fullRotations;
+
+  diff = diff > 0 ? //remove extra rotations so we dont cross the 0 mark an extra time
+    diff - (fullRotations * 100) :
+    diff + (fullRotations * 100);
+
+  var targetPos = position + diff;
+
+  if (position != 0 && (targetPos > 100 || targetPos < 0))
+  { count++; }
+
+  position = (100 + position + diff) % 100;
+  if (position == 0) count++;
+
+}
+System.Console.WriteLine("--------- PART 2 --------");
+System.Console.WriteLine(count);
+System.Console.WriteLine("-------------------------");
